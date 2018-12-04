c1088b0ebae65f5da1e12fac3a63ceb037ca5328
Jeffreywye <jeffreywye7@gmail.com>
2018-12-03 19:21:15 -0500

    Created Servlet Functionality to add a User with a username and password to the User DB from a servlet

--- Jobster/src/com/jster/servlet/RegisterServlet.java
+++ Jobster/src/com/jster/servlet/RegisterServlet.java
@@ -1,5 +1,6 @@
-package com.jster.servlets;
+package com.jster.servlet;
 import com.jster.beans.*;
+import com.jster.util.*;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -31,27 +32,53 @@ public class RegisterServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request,response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		
-		
+
 		//		step 1 set content type
 		response.setContentType("text/html");
 
 		//		step 2 get print writer
 		PrintWriter out = response.getWriter();
 		
-		//		step 3 generate html
-		out.println("<html><body>");
-//		out.print();
-		out.println("<a href=portal.html>Return to Home</a>");
-		out.println("</body></html>");
+		User sessUser = new User();
+		sessUser.setUser_name(request.getParameter("username"));
+		sessUser.setPassword(request.getParameter("password"));
+		
+//		System.out.println(sessUser.getUser_name());
+//		check if username is avilable
+		if(UserUtil.isUserNameAval(sessUser)){
+			UserUtil.commitNewUser(sessUser);
+			//		step 3 generate html
+			out.println("<html><body>");
+			out.println("User confirmed");
+			out.println("<br></br>");
+			out.println("<a href=portal.html>Return to Home</a>");
+			out.println("</body></html>");
+		}
+		else{
+//			step 3 generate html
+			out.println("<html><body>");
+			out.println("FAILED: UserName Taken");
+			out.println("<br></br>");
+			out.println("<a href=portal.html>Return to Home</a>");
+			out.println("</body></html>");
+		}
+		
+////		step 3 generate html
+//		out.println("<html><body>");
+//		out.println("Test confirmed");
+//		out.println("<br></br>");
+//		out.println("<a href=portal.html>Return to Home</a>");
+//		out.println("</body></html>");
+		
+
+		
 	}
 
 }
