e98ee1fa59260d750fe8060f0d7f03b66d64f6a0
Jeffreywye <jeffreywye7@gmail.com>
2018-12-03 19:23:43 -0500

    Added a mass reset sql to drop all tables but it DOES NOT DROP THE SEQUENCES

---
+++ Jobster/sql/MassResetSQL.sql
@@ -0,0 +1,11 @@
+Drop Table User_Preferences;
+Drop Table Job_User_Favorites;
+Drop Table Job_Applicants;
+Drop Table Job_Post_Categ;
+Drop Table Job_Post_Type;
+Drop Table Job_Category;
+Drop Table Job_Type;
+Drop Table User_Job_Location;
+Drop Table Job_Post;
+Drop Table Location;
+Drop Table Users;
